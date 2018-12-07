2dd74dd52a2058a843402fe09f38da4d5fe73089
zhaoliwe <zhaolw@runlion.com>
2018-12-07 15:25:18 +0800

    add nio 客户端

--- src/main/java/com/spring/boot/controller/netty/TimeClientNio.java
+++ src/main/java/com/spring/boot/controller/netty/TimeClientNio.java
@@ -1,5 +1,6 @@
 package com.spring.boot.controller.netty;
 
+import com.alibaba.fastjson.JSONObject;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import lombok.extern.slf4j.Slf4j;
 
@@ -8,15 +9,22 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
-public class TimeClient {
+public class TimeClientNio {
     static ExecutorService threadPool = new ThreadPoolExecutor(1330, 1350, 20, TimeUnit.SECONDS, new LinkedBlockingQueue<>(1300),
             new ThreadFactoryBuilder().setNameFormat("XX-task-%d").build());
+
+
+
+
+
 
     public static void main(String[] args) {
         int port = 8055;
@@ -30,10 +38,17 @@ public class TimeClient {
             e.printStackTrace();
         }
         Socket finalSocket = socket;
-        for (int j=1 ;j<999999;j++){
+        for(int i=0 ;i<1000;i++){
+            try {
+                 Thread.sleep(5000L);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            for (int j=1 ;j<999;j++){
 
 
-            int finalJ = j;
+                int finalJ = j;
+
             threadPool.execute(new Runnable() {
                @Override
                 public void run() {
@@ -41,48 +56,22 @@ public class TimeClient {
                 }
             });
         }
+        }
 
     }
 
     private static void sendByBio(Socket socket,int j) {
-        BufferedReader in = null;
-        PrintWriter out = null;
+
+
         try {
-           // Thread.sleep(1000L);
+           // socket = new Socket("192.168.158.173", 8055);
+            TimeClientHandleNio timeClientHandleNio = new TimeClientHandleNio("192.168.158.173", 8055);
+            log.info("完成" + JSONObject.toJSONString(timeClientHandleNio));
         } catch (Exception e) {
             e.printStackTrace();
         }
-        try {
-            socket = new Socket("192.168.158.173", 8055);
-            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            out = new PrintWriter(socket.getOutputStream(), true);
-            out.println("QUERY TIME ORDER"+j);
-            log.info("消息发送成功"+j);
-            String resp = in.readLine();
-            log.info("收到消息：" + resp);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (out != null) {
-                out.close();
-                out = null;
-            }
-            if (in != null) {
-                try {
-                    in.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                in = null;
-            }
-            try {
-                socket.close();
-                socket=null;
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
 
 
         }
-    }
+
 }
